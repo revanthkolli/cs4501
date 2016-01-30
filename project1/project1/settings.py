80458391b93eafb12519eb765f5b50b29941e04b
revanthkolli <revanthkolli@gmail.com>
2016-01-30 22:30:07 +0000

    hello world

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -103,3 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates')
+)
--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -16,5 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'accounts.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
